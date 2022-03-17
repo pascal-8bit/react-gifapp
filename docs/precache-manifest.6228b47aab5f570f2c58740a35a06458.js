ee29c2910b9901693ba2d8f271c619a005a5a46e
pascal <paolo.arcangeli@digitaliasistemi.it>
2022-03-17 11:35:12 +0100

    docs aggregati

---
+++ docs/precache-manifest.6228b47aab5f570f2c58740a35a06458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1d590f00964171480ff49e769ed273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d3616774d867c9b20ba",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3616774d867c9b20ba",
+    "url": "/static/js/main.aa1a8b45.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
